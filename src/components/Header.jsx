f830a4590209581e9703fc09ce194adb2b41bd50
Aleksey <denalexsr@yandex.ru>
2021-11-17 22:58:45 +0300

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return  <header className='header'>
+        <img src='https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258_960_720.png'/>
+        </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return  (
+        <div className='content'>
+            <div>
+                <img src='https://img.desktopwallpapers.ru/nature/pics/wide/1920x1200/849ec9470d6af3370b7526f08a3ef9dc.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
